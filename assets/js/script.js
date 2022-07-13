035b34d61be85ab55a08d465866545367ec5331a
Emily Hammon <ehammon7@gmail.com>
2022-07-12 22:51:58 -0400

    saves to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,6 +82,8 @@ var createTaskEl = function(taskDataObj) {
 
     //increase task counter for next unique id
     taskIdCounter++;
+
+    saveTasks();
 };
 
 var createTaskActions = function(taskId) {
@@ -165,6 +167,9 @@ var deleteTask = function(taskId) {
 
     // reassign tasks array to be the same as the updatedTaskArr
     tasks = updatedTaskArr;
+
+    saveTasks();
+    
 };   
 
 var editTask = function(taskId) {
@@ -205,6 +210,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
 
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
+
+    saveTasks();
 };
 
 var taskStatusChangeHandler = function(event) {
@@ -236,9 +243,12 @@ var taskStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks);
+  saveTasks();
 };
 
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
